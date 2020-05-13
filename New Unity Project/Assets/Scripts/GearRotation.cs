a74afda616e54c6eaefddc39cbc5222439c7bd1e
pizzasteave <41120300+pizzasteave@users.noreply.github.com>
2020-05-13 11:58:26 +0100

    gear system added + hologram
    no interaction with the gear system
    lightwight reneder pipleline required

---
+++ New Unity Project/Assets/Scripts/GearRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearRotation : MonoBehaviour
+{
+    public int speed;
+    public Rigidbody rb;
+    public Vector3 vector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vector = new Vector3(0, speed, 0);
+    }
+
+    // Update is called once per frame
+    public void FixedUpdate()
+    {
+        Quaternion deltaRotation = Quaternion.Euler(vector * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
+}
