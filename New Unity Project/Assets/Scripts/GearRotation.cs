fa5ad15e78018d96afa8eac78e4f3078ef802c23
pizzasteave <41120300+pizzasteave@users.noreply.github.com>
2020-06-03 22:41:38 +0100

    done ye si saif
    all good gear system problem solved

--- New Unity Project/Assets/Scripts/GearRotation.cs
+++ New Unity Project/Assets/Scripts/GearRotation.cs
@@ -23,16 +23,18 @@ public class GearRotation : MonoBehaviour
     public bool triggred;
 
     public bool doura;
-    public bool dourabar;
-
+  
     public Vector3 firstRotationVector ;
-    public Vector3 firstPositionVector; 
+    public Vector3 firstPositionVector;
+    public float _time;
+    private float frame; 
   
     // Start is called before the first frame update
     void Start()
     {
           doura = false;
-        dourabar = true; 
+     
+        frame = 0.02f;
 
         // Debug.Log(firstRotation = transform.localRotation) ;
 
@@ -49,7 +51,7 @@ public class GearRotation : MonoBehaviour
 
     }*/
     // Update is called once per frame
-    public void Update()
+    public void FixedUpdate()
     {
 
 
@@ -67,65 +69,67 @@ public class GearRotation : MonoBehaviour
          }*/
         if (triggred == true && doura == false   )
           {
+            _time = Math.Abs(_time - frame);
+            Debug.Log(_time); 
+            //StartCoroutine(ExampleCoroutine());
 
-            StartCoroutine(ExampleCoroutine()); 
+            if (_time >= 0.02f)
+            {
+                Debug.Log("here");
+                speed = rb.velocity.magnitude;
+                angularSpeed = rb.angularVelocity.magnitude * Mathf.Rad2Deg;
+                rb.angularVelocity = new Vector3(0, 0, Mathf.PI / 2);
+              
 
+            }
+
+            //  vector = new Vector3(0, speed, 0);
+
+
+            //   Quaternion deltaRotation = Quaternion.Euler(vector * Time.deltaTime);
+
+            //   rb.MoveRotation( rb.rotation *  deltaRotation);
+
+
+            //   Debug.Log( rb.rotation.eulerAngles.x); 
+
+
+            if (_time <= 0.02f)
+            {
+                Debug.Log("here22");
+
+                triggred = false;
+                Debug.Log(triggred);
+                Debug.Log("360");
+                doura = true;
+              
+
+
+                rb.angularVelocity = new Vector3(0, 0, 0);
+
+                Vector3 tmp = firstRotationVector;
+                transform.eulerAngles = tmp;
+
+                Vector3 tmp2 = firstPositionVector;
+                Debug.Log(transform.position);
+                transform.position = tmp2;
+                _time = 4; 
+               
+
+            }
         }
 
         doura = false;
-        dourabar = true;
-
-
-    }
-
-     public IEnumerator ExampleCoroutine()
-  {
-        speed = rb.velocity.magnitude;
-        angularSpeed = rb.angularVelocity.magnitude * Mathf.Rad2Deg;
-        rb.angularVelocity = new Vector3(0, 0, Mathf.PI/2);
  
-        //  vector = new Vector3(0, speed, 0);
 
 
-        //   Quaternion deltaRotation = Quaternion.Euler(vector * Time.deltaTime);
-
-        //   rb.MoveRotation( rb.rotation *  deltaRotation);
-
-
-      //   Debug.Log( rb.rotation.eulerAngles.x); 
-
-
-        
-        yield return new WaitForSecondsRealtime(4);
-     
-            ResetGear();
-       
-   }
-
-
-
-    public void ResetGear()
-    {
-
-        if (dourabar == true)
-        {
-            triggred = false;
-            Debug.Log(triggred);
-            Debug.Log("360");
-            doura = true;
-            dourabar = false;
-
-
-            rb.angularVelocity = new Vector3(0, 0, 0);
-
-            Vector3 tmp = firstRotationVector;
-            transform.eulerAngles = tmp;
-
-            Vector3 tmp2 = firstPositionVector;
-            Debug.Log(transform.position);
-            transform.position = tmp2;
-        }
-       
     }
+
+  /*   public IEnumerator ExampleCoroutine()
+  {
+   }*/
+
+
+
 
 }
